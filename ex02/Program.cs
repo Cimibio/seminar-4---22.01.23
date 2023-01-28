f3c43f9504e31eaf4ee3a5283d38aaa321a55320
AndreySh <shingaryovan@gmail.com>
2023-01-28 21:29:43 +1200

    добавлен пример с изменением порядка имен

---
+++ ex02/Program.cs
@@ -0,0 +1,22 @@
+﻿// поменять местами имена и фамилии
+Console.Write("введите имена: ");
+string s = Console.ReadLine()??"";
+void PrintMasString(string[] arr)
+{
+    foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+}
+string[] names = s.Split(" "); //создаем массив с делителем "пробел"
+
+PrintMasString(names);
+
+for (int i = 0; i < names.Length/2; i++)
+{
+    string temp = names[i];
+    names[i] = names[names.Length - 1 - i];
+    names[names.Length - 1 - i] = temp; 
+}
+
+PrintMasString(names);
