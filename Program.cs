d9c5d885b1ca800eaa745bf1bd07d55af148f49d
AndreySh <shingaryovan@gmail.com>
2023-01-28 21:14:29 +1200

    заполнение и сравнение элементов массива, нахождение количества максимально часто встречающего элемента

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Найти максимальное повторение "1" в массиве
+int[] array = new int[8];
+for (int i = 0; i <= 7; i++)
+{
+    array[i] = new Random().Next(0,2);
+}
+void PrintMas(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+PrintMas(array);
+int FindMaxCount(int[] arr)
+{
+    int result = 1;
+    int count = 1;
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        if (arr[i] == arr[i+1] && arr[i] == 1)
+        {
+            count++;
+            if (count > result) result = count;
+        }
+        else count = 0;
+    }
+    return result;
+}
+Console.WriteLine(FindMaxCount(array));
---
+++ ex01/Program.cs
@@ -0,0 +1,42 @@
+﻿// Дан одномерный массив, написать сколько раз повторяется самое частое число
+Console.WriteLine("введите количество элементов массива");
+int N = int.Parse(Console.ReadLine());
+int[] array = new int[N];
+void FillMas(int[] arr)
+{
+    for (int i = 0; i <= arr.Length - 1; i++)
+    {
+        arr[i] = new Random().Next(0,10);
+    }
+}
+FillMas(array);
+
+void PrintMas(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+PrintMas(array);
+
+int findNumber(int[] arr)
+{
+    int result = 1;
+    int count = 1;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        count = 1;
+        for (int j = i +1; j < arr.Length; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+                if (count > result) result = count;
+            }
+        }
+    }
+    return result;
+}
+Console.WriteLine(findNumber(array));   
